11134ad0844d3eae4ebac03afa9b901fde5e5393
Fernando Souza <nandosouzafilho@gmail.com>
2015-04-21 23:18:42 -0300

    Find max and find min methods

--- tree.js
+++ tree.js
@@ -5,6 +5,44 @@
 		this.root = null;
 	};
 
+	function _findeMin(tree) {
+
+		var currentTree = tree || this.root;
+
+		if (!currentTree) {
+			return;
+		}
+
+		if (currentTree.left != null) {
+			return _findeMin(currentTree.left);
+		} else {
+			return currentTree;
+		}
+	};
+
+	function _findeMax(tree) {
+
+		var currentTree = tree || this.root;
+
+		if (!currentTree) {
+			return;
+		}
+
+		if (currentTree.right != null) {
+			return _findeMax(currentTree.right);
+		} else {
+			return currentTree;
+		}
+	};
+
+	Tree.prototype.findeMin = function() {
+		return _findeMin(this.root);
+	};
+
+	Tree.prototype.findeMax = function() {
+		return _findeMax(this.root);
+	};
+
 	Tree.prototype.insertNode = function(value, tree) {
 		var currentTree,
 			directionKey;
@@ -32,4 +70,4 @@
 
 	window.Tree = Tree;
 
-})()
+})();
